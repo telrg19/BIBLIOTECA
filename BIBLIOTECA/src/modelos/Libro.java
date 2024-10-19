0dea80c8cccbb51c10ab0f0c854b3a6413808f51
Telma <Telma@192.168.1.35>
2024-10-19 17:33:40 +0200

    Creacion de libro, atributos de instancia y de clase. Closes #2

---
+++ BIBLIOTECA/src/modelos/Libro.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+/**
+ *
+ * @author Telma
+ */
+public class Libro {
+    //*****************************************************************
+    // Attributes 
+    //*****************************************************************
+    private String titulo;
+    private String genero;
+    private String autor;
+    private String editorial;
+    private int isbn;
+    private int fechaPublicacion;
+    
+    
+    
+    
+    
+    
+    
+}
