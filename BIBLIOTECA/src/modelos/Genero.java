5cfdc936bfe8b33730900740cc3216a2a3f0c3a4
Telma <Telma@192.168.1.35>
2024-10-21 11:37:53 +0200

    Termino los enums genero e idioma. Closes #11

---
+++ BIBLIOTECA/src/modelos/Genero.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package modelos;
+
+/**
+ *
+ * @author Telma
+ */
+public enum Genero {
+    NARRATIVA, CIENCIA_FICCION, POESIA, LIRICO, AVENTURAS, DRAMA, NOVELA, NOVELA_NEGRA, HISTORIA, 
+    JUVENIL, INFANTIL, NOVELA_GRAFICA, AUTOAYUDA, BIOGRAFIA, ROMANTICA, FANTASIA, DESCONOCIDO;
+}
